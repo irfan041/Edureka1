d6c7383fb98d2f775c7041e94c3c78241249f1c1
irfan041 <er.irfan041@gmail.com>
2022-09-21 01:07:26 -0400

    Add files via upload

---
+++ EurekaClient123/src/test/java/com/example/EurekaClient123/EurekaClient123ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EurekaClient123;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClient123ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
